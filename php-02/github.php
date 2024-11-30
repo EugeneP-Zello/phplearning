8e0512dbe5fcce2a82628febaf2e8b2abaafa4a9
Eugene Potapov <epotapov@zello.com>
2024-11-30 04:19:35 -0600

    github star check

---
+++ php-02/github.php
@@ -0,0 +1,41 @@
+<?php
+if(!empty($_GET["apiKey"])) {
+    $ch = curl_init();
+
+    $authHeader = "Authorization: token {$_GET['apiKey']}";
+
+    $headers = [
+        $authHeader,
+        "User-Agent: EugeneP-Zello"
+    ];
+    $url = "https://api.github.com/user/starred/EugeneP-Zello/phplearning";
+    curl_setopt_array($ch, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => $headers,
+    ]);
+
+    $result = curl_exec($ch);
+
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php if ((isset($http_status) ? $http_status : null) == 204): ?>
+    result: <?= $http_status ?> Starred!
+<?php endif; ?>
+<?php if ((isset($http_status) ? $http_status : null) == 404): ?>
+result: <?= $http_status ?> Not starred!
+<?php endif; ?>
+
+<form>
+    <label for="apiKey">Github API Key</label>
+    <input name="apiKey" id="apiKey" />
+    <button>Apply</button>
+</form>
+</body>
+</html>
